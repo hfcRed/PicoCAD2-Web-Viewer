22ce75dce82d97342d9fc8b9d838987f21604962
hfcRed <hfcred@gmx.net>
2026-03-24 18:56:47 +0100

    Add numeric control component

---
+++ src/lib/components/NumericControl.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { FormEventHandler } from 'svelte/elements';
+
+	interface Props {
+		label: string;
+		value: number;
+		min: number;
+		max: number;
+		step: number;
+		oninput?: FormEventHandler<HTMLInputElement>;
+	}
+
+	let { label, min, max, step, value = $bindable(), oninput }: Props = $props();
+
+	function updateValue(v: number) {
+		if (v === null) return v;
+		return Math.max(min, Math.min(max, v));
+	}
+</script>
+
+<div>
+	<label>
+		{label}
+		<input type="range" {min} {max} {step} bind:value {oninput} />
+	</label>
+	<input
+		type="number"
+		{min}
+		{max}
+		{step}
+		{oninput}
+		bind:value={() => value, (v) => (value = updateValue(v))}
+	/>
+</div>
+
+<style>
+	div {
+		display: grid;
+		grid-template-columns: 1fr calc(
+				(var(--pico-form-element-spacing-vertical) * 2) + (var(--pico-border-width) * 2) + 5ch
+			);
+		align-items: center;
+		gap: 0.5rem;
+
+		& input[type='number'] {
+			padding: var(--pico-form-element-spacing-vertical) 0.5rem;
+			font-family: var(--pico-font-family-monospace);
+			font-size: 0.8rem;
+		}
+
+		input::-webkit-outer-spin-button,
+		input::-webkit-inner-spin-button {
+			-webkit-appearance: none;
+			margin: 0;
+		}
+	}
+</style>
